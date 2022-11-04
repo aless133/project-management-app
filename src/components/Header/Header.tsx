ba7fd061d039b5ede3e96b35ba7eafe536021037
dimateniuta <dteniuta@gmail.com>
2022-11-04 20:52:46 +0300

    feat: add Button component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,11 @@ import styles from './Header.module.scss';
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <Button>SingUp</Button>
-      <Button>LogIn</Button>
+      <Button>Home</Button>
+      <div className={styles.wrapLogIn}>
+        <Button>SingUp</Button>
+        <Button>LogIn</Button>
+      </div>
     </header>
   );
 };
