ced3bf6d201dfd5c5fb46337e7c34c2b50d1baef
Mitya <partyzan327@gmail.com>
2022-11-04 17:38:05 +0300

    feat: Configure i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import en from './locales/en/translation.json';
+import ru from './locales/ru/translation.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
-    translation: {
-      'Learn React': 'Learn React1111',
-    },
+    translation: en,
   },
   ru: {
-    translation: {
-      'Learn React': 'Учите Реакт',
-    },
+    translation: ru,
   },
 };
 
@@ -21,12 +19,14 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    fallbackLng: 'en',
+    // debug: true,
+    detection: {
+      order: ['cookie'],
+      caches: ['cookie'],
+    },
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
     },
   });
 
