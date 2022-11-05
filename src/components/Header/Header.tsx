0d8f843c821929d75ebf43c5c6447b762204b493
dimateniuta <dteniuta@gmail.com>
2022-11-05 14:08:43 +0300

    feat: refactor: change way for adding color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,11 +9,25 @@ import {
   Switch,
   Box,
   Link,
+  createTheme,
+  ThemeProvider,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { House } from '@mui/icons-material';
 import { Trans, useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0288d1',
+    },
+    secondary: {
+      main: '#e3f2fd',
+      contrastText: 'blue',
+    },
+  },
+});
 
 export const Header = () => {
   const [lang, setLang] = useState(true);
@@ -28,89 +42,90 @@ export const Header = () => {
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexDirection: { xs: 'column', md: 'row' },
-        }}
-      >
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: { xs: 0, md: 1 } }}
-          >
-            <Link
-              component={NavLink}
-              sx={{
-                color: 'inherit',
-                textDecoration: 'none',
-                '&.active': {
-                  color: 'blue',
-                },
-              }}
-              to="/"
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" sx={{ color: 'primary.main' }}>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexDirection: { xs: 'column', md: 'row' },
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="menu"
+              sx={{ mr: { xs: 0, md: 1 }, color: 'secondary.main' }}
             >
-              <House sx={{ mt: 1 }} />
-            </Link>
-          </IconButton>
-          <Typography variant="h6" component="span" sx={{ flexGrow: 1 }}>
-            <Trans i18nKey={'welcome.home'}></Trans>
-          </Typography>
-        </Box>
+              <Link
+                component={NavLink}
+                sx={{
+                  color: 'inherit',
+                  textDecoration: 'none',
+                  '&.active': {
+                    color: 'secondary.contrastText',
+                  },
+                }}
+                to="/"
+              >
+                <House sx={{ mt: 1 }} />
+              </Link>
+            </IconButton>
+            <Typography variant="h6" component="span" sx={{ flexGrow: 1, color: 'secondary.main' }}>
+              <Trans i18nKey={'welcome.home'}></Trans>
+            </Typography>
+          </Box>
 
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <FormGroup>
-            <FormControlLabel
-              sx={{ mr: 3 }}
-              control={
-                <Switch
-                  checked={lang}
-                  onChange={handleChange}
-                  color="default"
-                  aria-label="login switch"
-                />
-              }
-              label={lang ? 'EN' : 'RU'}
-            />
-          </FormGroup>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <FormGroup>
+              <FormControlLabel
+                sx={{ mr: 3, color: 'secondary.main' }}
+                control={
+                  <Switch
+                    checked={lang}
+                    onChange={handleChange}
+                    color="default"
+                    aria-label="login switch"
+                  />
+                }
+                label={lang ? 'EN' : 'RU'}
+              />
+            </FormGroup>
 
-          <Button color="inherit" sx={{ width: 80, mr: 2 }}>
-            <Link
-              component={NavLink}
-              sx={{
-                color: 'inherit',
-                textDecoration: 'none',
-                '&.active': {
-                  color: 'blue',
-                },
-              }}
-              to="/auth"
-            >
-              <Trans i18nKey={'welcome.signin'}></Trans>
-            </Link>
-          </Button>
-          <Button color="inherit" sx={{ width: 105 }}>
-            <Link
-              component={NavLink}
-              sx={{
-                color: 'inherit',
-                textDecoration: 'none',
-                '&.active': {
-                  color: 'blue',
-                },
-              }}
-              to="/reg"
-            >
-              <Trans i18nKey={'welcome.signup'}></Trans>
-            </Link>
-          </Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
+            <Button sx={{ width: 80, mr: 2, color: 'secondary.main' }}>
+              <Link
+                component={NavLink}
+                sx={{
+                  color: 'inherit',
+                  textDecoration: 'none',
+                  '&.active': {
+                    color: 'secondary.contrastText',
+                  },
+                }}
+                to="/auth"
+              >
+                <Trans i18nKey={'welcome.signin'}></Trans>
+              </Link>
+            </Button>
+            <Button sx={{ width: 105, color: 'secondary.main' }}>
+              <Link
+                component={NavLink}
+                sx={{
+                  color: 'inherit',
+                  textDecoration: 'none',
+                  '&.active': {
+                    color: 'secondary.contrastText',
+                  },
+                }}
+                to="/reg"
+              >
+                <Trans i18nKey={'welcome.signup'}></Trans>
+              </Link>
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
