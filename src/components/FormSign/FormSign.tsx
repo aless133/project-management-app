6ec9de2ee02de7b5d82487dd05372cc4371b8140
aless133 <aless133@yandex.ru>
2022-11-07 23:25:22 +0300

    feat: center forms

--- src/components/FormSign/FormSign.tsx
+++ src/components/FormSign/FormSign.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+import Container from '@mui/system/Container';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import { useTranslation } from 'react-i18next';
-import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import Grid from '@mui/material/Grid';
+import styles from './formsign.module.scss';
 import { TErr, TValidator } from 'types';
 import {
   validateMaxLength,
@@ -24,7 +28,7 @@ const err: TErr = {
   [Constants.PASSWORD]: '',
 };
 
-export const FormSign = ({ isName = true }) => {
+export const FormSign = ({ isSignUp = true }) => {
   const [t] = useTranslation();
   const [inValid, setInValid] = useState(false);
   const [errStack, setErrStack] = useState<TErr>(err);
@@ -64,73 +68,76 @@ export const FormSign = ({ isName = true }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid2
-        sx={{ mt: '2rem' }}
-        container
-        spacing={4}
-        justifyContent="center"
-        alignItems="center"
-        direction="column"
-      >
-        <Grid2 xl={3} textAlign="center">
-          <Typography variant="h3" component="h2">
-            {isName ? t('Sign Up') : t('Sign In')}
-          </Typography>
-        </Grid2>
-        {isName && (
-          <Grid2 xl={3}>
-            <TextField
-              error={!!errStack.name}
-              name={Constants.NAME}
-              fullWidth
-              id="signUpLogin"
-              label={t('Name')}
-              defaultValue=""
-              helperText={t(errStack.name)}
-              onChange={handleChange}
-            />
-          </Grid2>
-        )}
-        <Grid2 xl={3}>
-          <TextField
-            error={!!errStack.login}
-            name={Constants.LOGIN}
-            fullWidth
-            id="signUpLogin"
-            label={t('Login')}
-            defaultValue=""
-            helperText={t(errStack.login)}
-            onChange={handleChange}
-          />
-        </Grid2>
-        <Grid2 xl={3}>
-          <TextField
-            error={!!errStack.password}
-            name={Constants.PASSWORD}
-            fullWidth
-            id="signUpLogin"
-            label={t('Password')}
-            defaultValue=""
-            helperText={t(errStack.password)}
-            onChange={handleChange}
-          />
-        </Grid2>
-        <Grid2 xl={3}>
-          <Button type="submit" disabled={inValid}>
-            {isName ? t('Sign Up') : t('Sign In')}
-          </Button>
-        </Grid2>
-        <Grid2>
-          <Link
-            style={{ textDecoration: 'none', color: 'inherit' }}
-            to={isName ? `${Constants.SIGN_IN}` : `${Constants.SIGN_UP}`}
-            replace
-          >
-            {isName ? t(`Do you have an account? Sign in`) : t(`Do you have an account? Sign up`)}
-          </Link>
-        </Grid2>
-      </Grid2>
-    </form>
+    <main className={styles.form_sign}>
+      <Container maxWidth="xl" sx={{ height: '100%' }}>
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+          <Grid item xl={4}>
+            <form onSubmit={handleSubmit}>
+              <Typography variant="h3" component="h2" align="center">
+                {isSignUp ? t('Sign Up') : t('Sign In')}
+              </Typography>
+              {isSignUp && (
+                <TextField
+                  error={!!errStack.name}
+                  name={Constants.NAME}
+                  fullWidth
+                  id="signUpLogin"
+                  label={t('Name')}
+                  defaultValue=""
+                  helperText={t(errStack.name)}
+                  onChange={handleChange}
+                  margin="normal"
+                />
+              )}
+              <TextField
+                error={!!errStack.login}
+                name={Constants.LOGIN}
+                fullWidth
+                id="signUpLogin"
+                label={t('Login')}
+                defaultValue=""
+                helperText={t(errStack.login)}
+                onChange={handleChange}
+                margin="normal"
+              />
+              <TextField
+                error={!!errStack.password}
+                name={Constants.PASSWORD}
+                fullWidth
+                id="signUpLogin"
+                label={t('Password')}
+                defaultValue=""
+                helperText={t(errStack.password)}
+                onChange={handleChange}
+                margin="normal"
+              />
+              <Button
+                type="submit"
+                disabled={inValid}
+                variant="contained"
+                fullWidth
+                size="large"
+                sx={{ mt: 2 }}
+              >
+                {isSignUp ? t('Sign Up') : t('Sign In')}
+              </Button>
+              <Typography align="center" sx={{ mt: 2 }}>
+                {isSignUp ? (
+                  <>
+                    <span>{t('Do you have an account?')}</span>{' '}
+                    <Link to={Constants.SIGN_IN}>{t('Sign in')}</Link>
+                  </>
+                ) : (
+                  <>
+                    <span>{t("Don't have an account?")}</span>{' '}
+                    <Link to={Constants.SIGN_UP}>{t('Sign up')}</Link>
+                  </>
+                )}
+              </Typography>
+            </form>
+          </Grid>
+        </Grid>
+      </Container>
+    </main>
   );
 };
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,10 +2,11 @@ import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import styles from './layout.module.scss';
 
 export const Layout = () => {
   return (
-    <div>
+    <div className={styles.layout}>
       <Header />
       <Outlet />
       <Footer />
