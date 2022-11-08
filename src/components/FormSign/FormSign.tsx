68642fe45f478c04bb9085e8bae7e47d3b0493ec
Mitya <partyzan327@gmail.com>
2022-11-08 14:57:47 +0300

    refactor: remove formSign hook

--- src/components/FormSign/FormSign.tsx
+++ src/components/FormSign/FormSign.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/system/Container';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -6,12 +6,85 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import styles from './formsign.module.scss';
 import { Link } from 'react-router-dom';
-import { useFormSign } from 'hooks/formSign.hook';
-import { Constants } from 'utils';
+import { Constants, isErrCheck, validateMaxLength, validateMinLength } from 'utils';
+import { IApiError, TErr, TValidator } from 'types';
+import { useTranslation } from 'react-i18next';
+import { useSignInMutation } from 'api/authApiSlice';
+import { useStoreDispatch } from 'hooks/store.hooks';
+import { set } from 'store/userSlice';
+
+const validator: TValidator = {
+  [Constants.NAME]: [
+    validateMinLength(Constants.MIN_LENGTH),
+    validateMaxLength(Constants.MAX_LENGTH),
+  ],
+  [Constants.LOGIN]: [
+    validateMinLength(Constants.MIN_LENGTH),
+    validateMaxLength(Constants.MAX_LENGTH),
+  ],
+  [Constants.PASSWORD]: [validateMinLength(Constants.PASSWORD_LENGTH)],
+};
+
+const err: TErr = {
+  [Constants.NAME]: '',
+  [Constants.LOGIN]: '',
+  [Constants.PASSWORD]: '',
+};
 
 export const FormSign = ({ isSignUp = true }) => {
-  const { errStack, inValid, isSigninLoading, t, handleSubmit, handleChange } =
-    useFormSign(isSignUp);
+  const [t] = useTranslation();
+  const [inValid, setInValid] = useState(false);
+  const [errStack, setErrStack] = useState<TErr>(err);
+  const [signin, { isLoading: isSigninLoading }] = useSignInMutation();
+  // const [signup, { isLoading: isSignupLoading }] = useSignUpMutation();
+  const dispatch = useStoreDispatch();
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    for (const [name, value] of formData.entries()) {
+      if (typeof value === 'string') {
+        err[name] = validator[name].reduce((acc, fn) => (acc += fn(value)), '');
+      }
+    }
+    setErrStack({ ...err });
+
+    if (!isErrCheck(err)) {
+      setInValid(false);
+
+      const data = Object.fromEntries(formData.entries());
+
+      if (isSignUp) {
+        alert('later');
+      } else {
+        try {
+          const signinData = await signin(data).unwrap();
+          const parseData = parseJwt(signinData.token);
+          dispatch(set({ token: signinData.token, id: parseData.userId, login: parseData.login }));
+        } catch (err) {
+          setErrStack({ submit: (err as IApiError).data.message });
+          // console.log('eeeee', err);
+        }
+      }
+    } else {
+      setInValid(true);
+    }
+  };
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target;
+
+    if (typeof value === 'string') {
+      err[name] = validator[name].reduce((acc, fn) => (acc += fn(value)), '');
+      setErrStack({ ...err });
+
+      if (!isErrCheck(err)) {
+        setInValid(false);
+      } else {
+        setInValid(true);
+      }
+    }
+  };
 
   return (
     <main className={styles.form_sign}>
@@ -90,3 +163,19 @@ export const FormSign = ({ isSignUp = true }) => {
     </main>
   );
 };
+
+function parseJwt(token: string) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+}
