524f0ed99593aae3be8ae50bc9a3c6e74b16c00d
Mitya <partyzan327@gmail.com>
2022-11-12 22:12:59 +0300

    feat: add styles for confirm modal

--- src/components/UI/ConfirmModal/ConfirmModal.tsx
+++ src/components/UI/ConfirmModal/ConfirmModal.tsx
@@ -1,5 +1,4 @@
-import { LoadingButton } from '@mui/lab';
-import { Button, CircularProgress } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ModalWindow } from '../ModalWindow';
@@ -28,27 +27,27 @@ export const ConfirmModal = ({ isOpen, onAction, onClose }: ConfirmModalProps) =
 
   return (
     <ModalWindow onClose={onClose} open={isOpen} title={t('You sure?')}>
-      <LoadingButton
-        loading={false}
-        loadingIndicator={<CircularProgress color="primary" size={25} />}
-        variant="contained"
-        fullWidth
-        size="large"
-        sx={{ mt: 2 }}
-        onClick={onAction}
-      >
-        {t('Confirm')}
-      </LoadingButton>
-      <Button
-        color="error"
-        variant="outlined"
-        fullWidth
-        size="large"
-        sx={{ mt: 2 }}
-        onClick={onClose}
-      >
-        {t('Cancel')}
-      </Button>
+      <Box sx={{ p: { md: 4 } }}>
+        <Button
+          variant="contained"
+          fullWidth
+          size="large"
+          sx={{ mt: 2, fontSize: { xs: 12 } }}
+          onClick={onAction}
+        >
+          {t('Confirm')}
+        </Button>
+        <Button
+          color="error"
+          variant="outlined"
+          fullWidth
+          size="large"
+          sx={{ mt: 2, fontSize: { xs: 12 } }}
+          onClick={onClose}
+        >
+          {t('Cancel')}
+        </Button>
+      </Box>
     </ModalWindow>
   );
 };
