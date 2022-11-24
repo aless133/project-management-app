c62c960ba8688d69b5769fbc9d3f038cb8345995
Mitya <partyzan327@gmail.com>
2022-11-24 15:03:25 +0300

    refactor: add stopPropagation to TrashBasket by default

--- src/pages/BoardPage/Task/Task.tsx
+++ src/pages/BoardPage/Task/Task.tsx
@@ -84,12 +84,7 @@ export const Task = ({ task, loading, openTaskModal }: ITaskProps) => {
             >
               <ModeEditOutlineOutlinedIcon />
             </IconButton>
-            <TrashBasket
-              onAction={(e) => {
-                e.stopPropagation();
-                handleDeleteTask(task._id);
-              }}
-            />
+            <TrashBasket onAction={() => handleDeleteTask(task._id)} />
           </Box>
         </Grid>
       )}
