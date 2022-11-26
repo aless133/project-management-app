18c8e564c29ba13cf2fb39ce21fecbcf2a23bd4c
dimateniuta <dteniuta@gmail.com>
2022-11-26 21:22:43 +0300

    rafactor: led to general appearance of update and delete buttons

--- src/components/UI/TrashBasket/TrashBasket.tsx
+++ src/components/UI/TrashBasket/TrashBasket.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import IconButton from '@mui/material/IconButton/IconButton';
 
 interface TrashBasketProps {
   onAction: () => void;
@@ -7,18 +8,21 @@ interface TrashBasketProps {
 
 export const TrashBasket = ({ onAction }: TrashBasketProps) => {
   return (
-    <DeleteOutlineIcon
-      sx={{
-        borderRadius: '50%',
-        color: 'gray',
-        p: 1,
-        ':hover': { backgroundColor: '#f4d8d8', color: '#dc5b5b', cursor: 'pointer' },
-        transition: 'background-color .3s',
-      }}
+    <IconButton
       onClick={(e) => {
         e.stopPropagation();
         onAction();
       }}
-    />
+      sx={{
+        color: 'gray',
+        ':hover': {
+          color: '#dc5b5b',
+          backgroundColor: '#f4d8d8',
+        },
+        transition: '0.3s',
+      }}
+    >
+      <DeleteOutlineIcon />
+    </IconButton>
   );
 };
