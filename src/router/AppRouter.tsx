452020f40d1b3b526dcc5f6dbceccf0723839474
aless133 <aless133@yandex.ru>
2022-11-28 00:50:45 +0300

    refactor: move suspense to ProtectedPage level

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,23 +3,23 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ProtectedPage from './ProtectedPage';
 
-import AccountPage from 'pages/AccountPage';
-import BoardPage from 'pages/BoardPage';
-import ErrorPage from 'pages/ErrorPage';
-import MainPage from 'pages/MainPage';
-import SignInPage from 'pages/SignInPage';
-import SignUpPage from 'pages/SignUpPage';
-import WelcomePage from 'pages/WelcomePage';
-import SearchPage from 'pages/SearchPage';
+// import AccountPage from 'pages/AccountPage';
+// import BoardPage from 'pages/BoardPage';
+// import ErrorPage from 'pages/ErrorPage';
+// import MainPage from 'pages/MainPage';
+// import SignInPage from 'pages/SignInPage';
+// import SignUpPage from 'pages/SignUpPage';
+// import WelcomePage from 'pages/WelcomePage';
+// import SearchPage from 'pages/SearchPage';
 
-// const AccountPage = React.lazy(() => import('pages/AccountPage'));
-// const BoardPage = React.lazy(() => import('pages/BoardPage'));
-// const ErrorPage = React.lazy(() => import('pages/ErrorPage'));
-// const MainPage = React.lazy(() => import('pages/MainPage'));
-// const SignInPage = React.lazy(() => import('pages/SignInPage'));
-// const SignUpPage = React.lazy(() => import('pages/SignUpPage'));
-// const WelcomePage = React.lazy(() => import('pages/WelcomePage'));
-// const SearchPage = React.lazy(() => import('pages/SearchPage'));
+const AccountPage = React.lazy(() => import('pages/AccountPage'));
+const BoardPage = React.lazy(() => import('pages/BoardPage'));
+const ErrorPage = React.lazy(() => import('pages/ErrorPage'));
+const MainPage = React.lazy(() => import('pages/MainPage'));
+const SignInPage = React.lazy(() => import('pages/SignInPage'));
+const SignUpPage = React.lazy(() => import('pages/SignUpPage'));
+const WelcomePage = React.lazy(() => import('pages/WelcomePage'));
+const SearchPage = React.lazy(() => import('pages/SearchPage'));
 
 // import {
 //   AccountPage,
